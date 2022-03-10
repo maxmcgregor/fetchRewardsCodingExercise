c45a0358e9ea0e90c78954bb505dd9743072cf6d
Max McGregor <maxmcgregor1@gmail.com>
2022-03-10 16:27:21 -0600

    bare bones framework generated

---
+++ FetchCodingExercise/src/main/java/com/fetchcodingexercise/FetchCodingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.fetchcodingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchCodingExerciseApplication.class, args);
+	}
+
+}
---
+++ FetchCodingExercise/src/test/java/com/fetchcodingexercise/FetchCodingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetchcodingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchCodingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
